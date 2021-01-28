132453a1ccf154246ee75d1f071cbc6f4cbe1789
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-01-28 12:34:48 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,37 @@
+/**
+ * ### Addition
+ * @param {number} a _first_ argument
+ * @param {number} b __second__ argument
+ *
+ * @returns {number} Addition of numbers 'a' and 'b'
+ *
+ * - bullet 1
+ * - bullet 2
+ *
+ * @throws If the arguments are not numbers.
+ */
+const add = (a, b) => {
+  return a + b;
+};
+
+/**
+ * ### Subtraction
+ * @param {number} a _first_ argument
+ * @param {number} b __second__ argument
+ *
+ * @returns {number} Subtraction of numbers 'a' and 'b'
+ *
+ *
+ * @throws If the arguments are not numbers.
+ */
+const sub = (a, b) => {
+  return a - b;
+};
+
+/**
+ * ## Does the math
+ */
+const math = () => {
+  add(1, 2);
+  sub();
+};
